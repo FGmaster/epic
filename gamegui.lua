a01e5f91466c938d2bf556237fef6ced92fec440
FGmaster <130631294+FGmaster@users.noreply.github.com>
2023-05-01 13:29:52 -0400

    Create gamegui.lua

---
+++ gamegui.lua
@@ -0,0 +1,9 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("BA GUI", "Ocean")
+
+
+--NEW
+local Main = Window:NewTab("Main")
+local MainSection = Main:NewSection("Main")
+
+
